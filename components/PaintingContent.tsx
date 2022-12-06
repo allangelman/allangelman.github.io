34ee6bcb0afaff878ba83b0778a6f3865cdc80dc
Allan Gelman <allangelman4@gmail.com>
2022-12-06 02:55:54 -0500

    paintings layout

--- components/PaintingContent.tsx
+++ components/PaintingContent.tsx
@@ -9,19 +9,58 @@ interface imageLoaderProps{
 }
 
 const myLoader = ({ src, width, quality }: imageLoaderProps): string => {
-  return `https://allan-gelman.imgix.net/${src}?w=${width}&q=${quality || 75}`
+  return `https://allan-gelman.imgix.net/${src}?=${width}&q=${quality || 75}`
 }
 
 
 export const PaintingContent = () => {
   return (
     <Layout>
-    <div className="">
-      <Image src="/paintings/kaz.png" loader={myLoader} alt="kaz" width={2880} height={1300} />
-      <Image src="/paintings/natasha.png" loader={myLoader} alt="natasha" width={1409} height={1920} />
-      <Image src="/paintings/max.JPG" loader={myLoader} alt="max" width={300} height={500} />
-    </div>
-  </Layout>
+      <>
+      <div className ="flex space-x-4">
+        <div className="w-1/2">
+          <div className="mt-4">
+            <Image src="/paintings/kaz.png" loader={myLoader} alt="max" width={600} height={500} />
+          </div>
+          <div className="mt-4">
+            <Image src="/paintings/bentaylor.png" loader={myLoader} alt="max" width={600} height={500} />
+          </div>
+          <div className="mt-4">
+            <Image src="/paintings/natasha.png" loader={myLoader} alt="max" width={600} height={500} />
+          </div>
+        </div>
+        <div className="w-1/2">
+          <div className="mt-4">
+            <Image src="/paintings/meilin.png" loader={myLoader} alt="max" width={600} height={500} />
+          </div>
+          <div className="mt-4">
+            <Image src="/paintings/sun.png" loader={myLoader} alt="max" width={600} height={500} />
+          </div>
+          <div className="flex space-x-4">
+            <div className="w-1/2">
+              <div className="mt-4">
+                <Image src="/paintings/kala.jpg" loader={myLoader} alt="max" width={600} height={500} />
+              </div>
+            </div>
+            <div className="w-1/2">
+              <div className="mt-4">
+                <Image src="/paintings/max.JPG" loader={myLoader} alt="max" width={600} height={500} />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="w-1/3">
+          <div className="mt-4">
+            <Image src="/paintings/bentaylor.png" loader={myLoader} alt="max" width={400} height={500} />
+          </div>
+          <div className="mt-4">
+            <Image src="/paintings/max.JPG" loader={myLoader} alt="max" width={400} height={500} />
+          </div>
+        </div> */}
+
+      </div>
+      </>
+    </Layout>
   )
 }
 
