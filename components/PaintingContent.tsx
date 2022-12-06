901da6d54b9f2bd01097e6096877d4ce07473941
Allan Gelman <allangelman4@gmail.com>
2022-12-06 01:18:32 -0500

    image loader

--- components/PaintingContent.tsx
+++ components/PaintingContent.tsx
@@ -1,17 +1,25 @@
 import { Layout } from './Layout'
 import Image from 'next/image'
 
+
+interface imageLoaderProps{
+  src: string
+  width: number
+  quality?: number
+}
+
+const myLoader = ({ src, width, quality }: imageLoaderProps): string => {
+  return `https://allan-gelman.imgix.net/${src}?w=${width}&q=${quality || 75}`
+}
+
+
 export const PaintingContent = () => {
   return (
     <Layout>
     <div className="">
-      <span >
-        <Image src="/kaz.png" alt="Kaz" width={2880} height={1300} />
-      </span>
-      <span >
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </span>
-      <img src="/kaz.png" alt="Kaz" width={2880} height={1300} />
+      <Image src="/paintings/kaz.png" loader={myLoader} alt="kaz" width={2880} height={1300} />
+      <Image src="/paintings/natasha.png" loader={myLoader} alt="natasha" width={1409} height={1920} />
+      <Image src="/paintings/max.JPG" loader={myLoader} alt="max" width={300} height={500} />
     </div>
   </Layout>
   )
