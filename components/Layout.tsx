6fcaaea4ff600f568dade51dc6548fa4c578e700
Allan Gelman <allangelman4@gmail.com>
2022-12-06 22:38:27 -0500

    more paintings

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 }
 export const  Layout = ({children}: LayoutProps) => {
   return (
-      <div className="px-4 md:px-12 lg:px-32 pb-8">
+      <div className="px-4 md:px-12 lg:mx-auto lg:max-w-[1184px] pb-8">
         <Header/>
         {children}
         <Footer/>
--- components/PaintingContent.tsx
+++ components/PaintingContent.tsx
@@ -16,41 +16,33 @@ const myLoader = ({ src, width, quality }: imageLoaderProps): string => {
 export const PaintingContent = () => {
   return (
     <Layout>
-      <>
-      <div className ="flex space-x-4">
-        <div className="w-1/2">
-          <div className="mt-4">
+      <div className="grid gap-4 grid-cols-2">
+        <div>
+          <div className="mt-4" style={{borderRadius: '8px', overflow: 'hidden'}}>
             <Image src="/paintings/kaz.png" loader={myLoader} alt="max" width={600} height={500} />
           </div>
-          <div className="mt-4">
-            <Image src="/paintings/bentaylor.png" loader={myLoader} alt="max" width={600} height={500} />
-          </div>
-          <div className="mt-4">
+          <div className="mt-4" style={{borderRadius: '8px', overflow: 'hidden'}}>
             <Image src="/paintings/natasha.png" loader={myLoader} alt="max" width={600} height={500} />
           </div>
-        </div>
-        <div className="w-1/2">
-          <div className="mt-4">
-            <Image src="/paintings/meilin.png" loader={myLoader} alt="max" width={600} height={500} />
-          </div>
-          <div className="mt-4">
+          <div className="mt-4" style={{borderRadius: '8px', overflow: 'hidden'}}>
             <Image src="/paintings/sun.png" loader={myLoader} alt="max" width={600} height={500} />
           </div>
-          <div className="mt-4">
-            <Image src="/paintings/kalamax.png" loader={myLoader} alt="kalamax" width={600} height={500} />
+        </div>
+        <div>
+          <div className="mt-4" style={{borderRadius: '8px', overflow: 'hidden'}}>
+            <Image src="/paintings/meilin.png" loader={myLoader} alt="max" width={600} height={500} />
+          </div>
+          <div className="mt-4" style={{borderRadius: '8px', overflow: 'hidden'}}>
+            <Image src="/paintings/bentaylor.png" loader={myLoader} alt="max" width={600} height={500} />
+          </div>
+          <div className="mt-4" style={{borderRadius: '8px', overflow: 'hidden'}}>
+            <Image src="/paintings/L.png" loader={myLoader} alt="max" width={600} height={500} />
+          </div>
+          <div className="mt-4" style={{borderRadius: '8px', overflow: 'hidden'}}>
+            <Image src="/paintings/Pavement.jpg" loader={myLoader} alt="max" width={600} height={500} />
           </div>
         </div>
-        {/* <div className="w-1/3">
-          <div className="mt-4">
-            <Image src="/paintings/bentaylor.png" loader={myLoader} alt="max" width={400} height={500} />
-          </div>
-          <div className="mt-4">
-            <Image src="/paintings/max.JPG" loader={myLoader} alt="max" width={400} height={500} />
-          </div>
-        </div> */}
-
       </div>
-      </>
     </Layout>
   )
 }
