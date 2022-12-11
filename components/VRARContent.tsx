95476d0b158e73ab6f003a28c88e147a3cf29b5f
Allan Gelman <allangelman4@gmail.com>
2022-12-11 02:00:07 -0500

    cleanup

--- components/VRARContent.tsx
+++ components/VRARContent.tsx
@@ -4,24 +4,13 @@ import { Grid, Thumbnail } from "./Grid";
 export const VRARContent = () => {
   return (
     <Layout>
-      <>
-        {/* <script
-          type="text/javascript"
-          src="../balls-6e6c28174c59bf343edb10a27dc976aab744563e89fc5418cf114d9d3a0a49ea.js"
-        ></script> */}
-        {/* <script
-          type="text/javascript"
-          src="../Coding-432615b4e6c1f109.js"
-        ></script> */}
-        <div id="app" className="w-full h-[600px]"></div>
-      </>
-      {/* <Grid>
+      <Grid>
         <>
           <Thumbnail />
           <Thumbnail />
           <Thumbnail />
         </>
-      </Grid> */}
+      </Grid>
     </Layout>
   );
 };
